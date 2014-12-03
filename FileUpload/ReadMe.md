b475f6486775d7b156a783cedf3d69fb701f55a9
jgreenrise <jatindpatel@live.com>
2014-12-03 11:16:03 -0800

    Final commit: File upload

---
+++ FileUpload/ReadMe.md
@@ -0,0 +1,5 @@
+Reference
+http://www.concretepage.com/spring-4/spring-4-mvc-single-multiple-file-upload-example-with-tomcat
+
+http://localhost:8080/FileUpload/singleUpload
+http://localhost:8080/FileUpload/multipleUpload
---
+++ FileUpload/src/main/java/AppConfig.java
@@ -0,0 +1,39 @@
+
+
+import javax.servlet.MultipartConfigElement;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+@EnableAutoConfiguration
+public class AppConfig
+{
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement()
+    {
+	MultipartConfigFactory factory = new MultipartConfigFactory();
+	factory.setMaxFileSize("128KB");
+	factory.setMaxRequestSize("128KB");
+	return factory.createMultipartConfig();
+    }
+
+    @Bean
+    public UrlBasedViewResolver setupViewResolver()
+    {
+	UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+	resolver.setPrefix("/views/");
+	resolver.setSuffix(".jsp");
+	resolver.setViewClass(JstlView.class);
+	return resolver;
+    }
+}
